7e4814d5eea37dbd3fe67437871303a4f4e4cdaf
joniborda <jonathanmatiasborda@gmail.com>
2018-08-31 16:28:28 -0300

    Optimizo el código
    Itero las posiciones de la casa al mismo timpo que evaluo si hay pedregales

--- src/edu/unlam/pedregal/Pedregal.java
+++ src/edu/unlam/pedregal/Pedregal.java
@@ -11,16 +11,18 @@ public class Pedregal {
 		File salida = new File(path + "pedregal.out");
 		Scanner scEntrada = new Scanner(entrada);
 		Scanner scSalida = new Scanner(salida);
+
 		if (scSalida.next().equals("NO")) {
+			System.out.println("NO PUEDE IDENTIFICAR SI ES CORRECTO");
 			scEntrada.close();
 			scSalida.close();
 			return;
 		}
 
-		int dx = Integer.valueOf(scEntrada.next());
-		int dy = Integer.valueOf(scEntrada.next());
-		int frente = Integer.valueOf(scEntrada.next());
-		int fondo = Integer.valueOf(scEntrada.next());
+		int dimensionX = Integer.valueOf(scEntrada.next());
+		int dimensionY = Integer.valueOf(scEntrada.next());
+		int casaX = Integer.valueOf(scEntrada.next());
+		int casaY = Integer.valueOf(scEntrada.next());
 		int cantidadPedregales = scEntrada.nextInt();
 		scEntrada.nextLine();// LEE UNA LINEA EN BLANCO Y NO SE
 
@@ -30,42 +32,35 @@ public class Pedregal {
 			pedregales[i] = scEntrada.nextLine();
 		}
 
-		int aristaX = Integer.valueOf(scSalida.next());
-		int aristaY = Integer.valueOf(scSalida.next());
+		int inferiorX = Integer.valueOf(scSalida.next());
+		int inferiorY = Integer.valueOf(scSalida.next());
 		char orientacion = scSalida.next().charAt(0);
 
-		String[] casa = new String[frente * fondo];
-		int pos = 0;
+		String[] casa = new String[casaX * casaY];
 		if (orientacion == 'N' || orientacion == 'S') {
-			for (int i = 0; i < frente; i++) {
-				for (int j = 0; j < fondo; j++) {
-					casa[pos] = String.valueOf(aristaX + i) + ' ' + String.valueOf(aristaY + j);
-					pos++;
-				}
-			}
-		} else {
-			for (int i = 0; i < fondo; i++) {
-				for (int j = 0; j < frente; j++) {
-					casa[pos] = String.valueOf(aristaX + i) + ' ' + String.valueOf(aristaY + j);
-					pos++;
+			int aux = casaX;
+			casaX = casaY;
+			casaY = aux;
+		}
+
+		// ME FIJO SI LOS PEDREGALES ESTAN EN LA CASA EMPEZANDO POR LA X
+		for (int i = 0; i < casaY; i++) {
+			for (int j = 0; j < casaX; j++) {
+				for (int o = 0; o < pedregales.length; o++) {
+					if (pedregales[o].equals(String.valueOf(inferiorX + i) + ' ' + String.valueOf(inferiorY + j))) {
+						System.out.println("SALIDA INCORRECTA");
+						return;
+					}
 				}
 			}
 		}
 
-		for (int i = 0; i < casa.length; i++) {
-			for (int j = 0; j < pedregales.length; j++) {
-				if (casa[i].equals(pedregales[j])) {
-					System.out.println("SALIDA INCORRECTA");
-					return;
-				}
-			}
-		}
-
+		// SI LA CASA SE VA DEL TERRENO
 		for (int i = 0; i < casa.length; i++) {
 			String[] valores = casa[i].split(" ");
 			int x = Integer.valueOf(valores[0]);
 			int y = Integer.valueOf(valores[1]);
-			if (x > dx || y > dy) {
+			if (x > dimensionX || y > dimensionY) {
 				System.out.println("SALIDA INCORRECTA");
 				return;
 			}
